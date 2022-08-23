0b9911b921f36d132505261b1e526589fee974cd
Amk Lass <lassanaa14@gmail.com>
2022-08-23 01:21:35 +0100

    mission structure

---
+++ src/pages/Missions.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Missions from '../components/Missions/Missions';
+
+const MissionPage = () => (
+  <div className="container">
+    <Missions />
+  </div>
+);
+
+export default MissionPage;
---
+++ src/pages/Rockets.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import RocketsList from '../components/Rockets/RocketsList';
+import '../components/Rockets/Rockets.css';
+
+const RocketsPage = () => (
+  <div className="container">
+    <RocketsList />
+  </div>
+);
+
+export default RocketsPage;
