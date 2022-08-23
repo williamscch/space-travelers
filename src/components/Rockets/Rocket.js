54ddeb4619488bc9cf71e1ca521841f782db9ae8
Amk Lass <lassanaa14@gmail.com>
2022-08-23 18:05:55 +0100

    fix linters

--- src/components/Rockets/Rocket.js
+++ src/components/Rockets/Rocket.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
-import { cancelRockets, reserveRockets } from '../../redux/rockets.js/rockets';
 import './Rockets.css';
 
 const Rocket = (props) => {
