42f4be5d39528c99c5977ed0f4787eed6a80c955
Amk Lass <lassanaa14@gmail.com>
2022-08-23 17:50:08 +0100

    fix linters

--- src/App.js
+++ src/App.js
@@ -5,14 +5,12 @@ import './App.css';
 import Nav from './components/Nav';
 import MyProfile from './components/profile/profile';
 import MissionPage from './pages/Missions';
-import RocketPage from './pages/Rockets';
 
 function App() {
   return (
     <Router>
       <Nav />
       <Routes>
-        <Route path="/" element={<RocketPage />} />
         <Route path="*" element={<Navigate to="/" />} />
         <Route path="/missions" element={<MissionPage />} />
         <Route path="/myProfile" element={<MyProfile />} />
