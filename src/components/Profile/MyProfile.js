4aac59c2bbaf606162190dd795a1a70bff8c4afc
Amk Lass <lassanaa14@gmail.com>
2022-08-24 14:28:39 +0100

    missions and profile js update

--- src/components/Profile/MyProfile.js
+++ src/components/Profile/MyProfile.js
@@ -1,10 +1,28 @@
 import ReservedRockets from './ReservedRockets';
 import './MyProfile.css';
+import { useSelector } from 'react-redux';
 
 function MyProfile() {
+  const missions = useSelector((state) => state.missions);
   return (
     <div className="my-profile">
-      <ReservedRockets />
+       <section className="missions">
+        <h2>My Missions</h2>
+        <table className="missions-table">
+          <tbody>
+            {missions
+              && missions
+                .filter((mission) => mission.reserved === true)
+                .map((reserve) => (
+                  <tr className="row" key={reserve.missionId}>
+                    <td>{reserve.missionName}</td>
+                  </tr>
+                ))}
+          </tbody>
+        </table>
+      </section>
+      <section> <ReservedRockets/></section>
+     
     </div>
   );
 }
